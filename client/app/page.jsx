579ede35c54aeae11a7cbe7db76918dd69b9a860
user.name like : "workwithyuvraj0712@gmail.com <workwithyuvraj0712@gmail.com>
2024-09-26 15:37:06 +0530

    app page

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -5,16 +5,19 @@ import Page from './constructionalpraxis/page';
 import Speaking from './speaking/page';
 import Ideational from './ideationalpraxis/page';
 import Comprehension from './comprehension/page';
+import TaskHeading from './components/TaskHeading/page';
+import Subheading from './components/SubHeading/page';
+import Button from './components/Button/page';
 // import Wordrecall from './components/Wordrecall/page';
 
 export default function MainPage() {
   return (
     <>
-      <div className="flex justify-center items-center min-h-[calc(100vh-64px)]">
-        {/* <WordRecall/> */}
-        {/* <Ideational/>*/}
-        <Comprehension/>
-        
+      <div className="flex flex-col justify-center items-center min-h-[calc(100vh-64px)]">
+      <TaskHeading  heading="Alzheimer's Cognitive Assessment"/>
+      <Subheading subHeading='Sub'/>
+
+        <Button name="Start the test"/>
       </div>
     </>
   );
